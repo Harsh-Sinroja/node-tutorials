df4eac49e04ed0f22937a97b76da8fddad064456
Harsh-Sinroja <botbx14@gmail.com>
2024-10-24 17:38:44 +0530

    Little Changes

--- index.js
+++ index.js
@@ -11,44 +11,29 @@ app.get("/", async (req, res) => {
 })
 
 app.post("/", async (req, res) => {
-    let {email} = req.body;
-    let user = await User.findOne({email})
+    let { email } = req.body;
+    let user = await User.findOne({ email })
 
-    if(user){
+    if (user) {
         return res.send("user Already Exist")
     }
-    else{
+    else {
         let data = await User.create(req.body)
         res.send(data);
     }
 })
 
-app.post("/login",async(req,res)=>{
-    let {email,password}=req.body;
-    let user=await User.findOne({email,password});
-
-    if(!user){
-        return res.send("User does not exists");
-    }
-
-    if(password!=user.password){
-        return res.send("Data credential does not match");
-    }
-
-    res.send("Login successfully");
-})
-
 app.put("/:id", async (req, res) => {
     let { id } = req.params;
-    const data = await User.findByIdAndUpdate(id,req.body,{new:true});
+    const data = await User.findByIdAndUpdate(id, req.body, { new: true });
     res.send(data)
 })
 
-// app.patch("/patch/:id", async (req, res) => {
-//     let { id } = req.params;
-//     const data = await User.findByIdAndUpdate(id, req.body, { new: true });
-//     res.send(data)
-// })
+app.patch("/patch/:id", async (req, res) => {
+    let { id } = req.params;
+    const data = await User.findByIdAndUpdate(id, req.body, { new: true });
+    res.send(data)
+})
 
 app.delete("/:id", async (req, res) => {
     let { id } = req.params;
@@ -56,30 +41,52 @@ app.delete("/:id", async (req, res) => {
     res.send("User deleted successfullly!");
 })
 
-app.get("/products",async(req,res)=>{
+app.post("/login", async (req, res) => {
+    let { email, password } = req.body;
+    let user = await User.findOne({ email, password });
+
+    if (!user) {
+        return res.send("User does not exists");
+    }
+
+    if (password != user.password) {
+        return res.send("Data credential does not match");
+    }
+
+    res.send("Login successfully");
+})
+
+app.get("/products", async (req, res) => {
     const products = await Product.find();
     res.send(products);
 })
 
-app.post("/products",async(req,res)=>{
-    let data = await Product.create(req.body)
+app.post("/products", async (req, res) => {
+    let { name } = req.body;
+    const product = Product.find({ name });
+
+    if (product) {
+        return res.send("Product Already Exists");
+    } else {
+        let data = await Product.create(req.body)
+        res.send(data);
+    }
+})
+
+app.put("/products/:id", async (req, res) => {
+    let { id } = req.params;
+    let data = await Product.findByIdAndUpdate(id, req.body, { new: true });
     res.send(data);
 })
 
-app.put("/products/:id",async(req,res)=>{
-    let {id} = req.params;
-    let data = await Product.findByIdAndUpdate(id,req.body,{new:true});
+app.patch("/products/patch/:id", async (req, res) => {
+    let { id } = req.params;
+    let data = await Product.findByIdAndUpdate(id, req.body, { new: true });
     res.send(data);
 })
 
-app.patch("/products/patch/:id",async(req,res)=>{
-    let {id} = req.params;
-    let data = await Product.findByIdAndUpdate(id,req.body,{new:true});
-    res.send(data);
-})
-
-app.delete("/products/:id",async(req,res)=>{
-    let {id} = req.params;
+app.delete("/products/:id", async (req, res) => {
+    let { id } = req.params;
     await Product.findByIdAndDelete(id);
     res.send("Product deleted successfully!");
 })
